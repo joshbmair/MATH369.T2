13bf3afc96556a3aec656fe9a0e4c93c38912989
Josh Birlingmair <jcbmair@gmail.com>
2020-12-15 22:22:11 -0700

    Added function that finds the set of prime factors of a number

--- T2.py
+++ T2.py
@@ -1,3 +1,18 @@
 import sys
 import math
 
+def get_prime_factors(n):
+    factor_set = set()
+    o = n
+
+    for index in range(n):
+        if index > 1:
+            while o % index == 0:
+                factor_set.add(index)
+                o = o / index
+
+    return factor_set
+
+if __name__ == "__main__":
+    factor_set = get_prime_factors(213)
+    print(factor_set)
